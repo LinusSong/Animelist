0fc9750a811319ba81575d7a61b3d9fd73f13fa0
LinusSong <linustrue@gmail.com>
2015-06-13 02:55:22 +0800

    Update

--- animelist.py
+++ animelist.py
@@ -1,111 +1,90 @@
 #-*-coding:utf-8 -*-
 import re
-import urllib
-import urllib2
-#urllink = "http://share.dmhy.org/cms/page/name/programme.html"
-#user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:37.0) Gecko/20100101 Firefox/37.0'
-#headers = { 'User-Agent' : user_agent }
-#req = urllib2.Request(urllink, None, headers)
-#response = urllib2.urlopen(req)
-#pagecontent = response.read()
-#urllib.urlretrieve ("http://share.dmhy.org/cms/page/name/programme.html","anime.html")
-page = open("anime.html")
-pagecontent = page.read()
+import yaml
+import requests
+
+def parsepage(url):
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'
+    headers1 = { 'User-Agent' : user_agent }
+    page = requests.get(url, headers=headers1).text.encode('utf8')
+    return page
+
+def conv_Weekday2Chinese(attr):
+    if attr == "mon":
+        weekday = '星期一'
+    elif attr == "tue":
+        weekday = '星期二'
+    elif attr == "wed":
+        weekday = '星期三'
+    elif attr == "thu":
+        weekday = '星期四'
+    elif attr == "fri":
+        weekday = '星期五'
+    elif attr == "sat":
+        weekday = '星期六'
+    elif attr == "sun":
+        weekday = '星期日'
+    return weekday
+
+pagecontent = parsepage("http://share.dmhy.org/cms/page/name/programme.html")
+target_all = file("all.yml","w")
+target_remain = file("remain.yml","w")
+target_firstselect = file("firstselect.yml","w")
+target_select = file("select.yml","w")
 pagecontent = pagecontent[pagecontent.find('//其它'):pagecontent.find('IE新窗')]
-urls = re.findall('.{3}array.*<a href=.*<\/a>',pagecontent)
-target = open("animeselect1.html","w")
-target2 = open("animeselect2.html","w")
-for i in urls:
-    i = re.sub('array.push\(\[\'[a-zA-z]+://[^\s,]*','',i)
-    i = re.sub('\'','',i)
-    unicodetemp = i.find(",")
-    unicodeleft = i.find(",",unicodetemp + 1)
-    unicoderight = i.find(",",unicodeleft + 1)
-    i = i[:unicodeleft]+i[unicoderight:]
-    i = i.replace('/topics/list','http://share.dmhy.org/topics/rss/rss.xml')
-    if i[0:3] == "mon":
-        i = i.replace('mon','星期一')
-    if i[0:3] == "tue":
-        i = i.replace('tue','星期二')
-    if i[0:3] == "wed":
-        i = i.replace('wed','星期三')
-    if i[0:3] == "thu":
-        i = i.replace('thu','星期四')
-    if i[0:3] == "fri":
-        i = i.replace('fri','星期五')
-    if i[0:3] == "sat":
-        i = i.replace('sat','星期六')
-    if i[0:3] == "sun":
-        i = i.replace('sun','星期日')
-    rssaddress = re.findall('<a href=[^>]*\>[^<]*\</a\>',i)
-    commaone = i.find(",")
-    commatwo = i.find(",",commaone + 1)
-    # 極影
-    if i.find("team_id%3A185\"") != -1:
-        for a in rssaddress:
-            if a.find("team_id%3A185\"") != -1:
-                pos = a.find( "\">" )
-                content = a[:pos] + "+GB+720+MP4&sort_id=2" + a[pos:]
-                target.write(i[:commatwo+1] + content + '<br>')
-                rsstest = re.findall('http://[^"]*',content)
-#                if urllib.urlopen(rsstest[0]).read().find("<item>") == -1:
-#                    target.write("This rss address is wrong. Please check it manually.<br>")
-                target.write('\n')
-    # 華盟
-    elif i.find("team_id%3A49\"") != -1:
-        for a in rssaddress:
-            if a.find("team_id%3A49\"") != -1:
-                pos = a.find( "\">" )
-                content = a[:pos] + "+%E7%AE%80%E4%BD%93+720+MP4&sort_id=2" + a[pos:]
-                target.write(i[:commatwo+1] + content + '<br>')
-                rsstest = re.findall('http://[^"]*',content)
-#                if urllib.urlopen(rsstest[0]).read().find("<item>") == -1:
-#                    target.write("This rss address is wrong. Please check it manually.<br>")
-                target.write('\n')
-    # 千夏
-    elif i.find("team_id%3A283\"") != -1:
-        for a in rssaddress:
-            if a.find("team_id%3A283\"") != -1:
-                pos = a.find( "\">" )
-                content = a[:pos] + "+%E7%B9%81%E9%AB%94+720+MP4&sort_id=2" + a[pos:]
-                target.write(i[:commatwo+1] + content + '<br>')
-                rsstest = re.findall('http://[^"]*',content)
-#                if urllib.urlopen(rsstest[0]).read().find("<item>") == -1:
-#                    target.write("This rss address is wrong. Please check it manually.<br>")
-                target.write('\n')
-    #輕之國度
-    elif i.find("team_id%3A321\"") != -1:
-        for a in rssaddress:
-            if a.find("team_id%3A321\"") != -1:
-                pos = a.find( "\">" )
-                content = a[:pos] + "+GB+720+MP4&sort_id=2" + a[pos:]
-                target.write(i[:commatwo+1] + content + '<br>')
-                rsstest = re.findall('http://[^"]*',content)
-#                if urllib.urlopen(rsstest[0]).read().find("<item>") == -1:
-#                    target.write("This rss address is wrong. Please check it manually.<br>")
-                target.write('\n')
-    #澄空
-    elif i.find("team_id%3A58\"") != -1:
-        for a in rssaddress:
-            if a.find("team_id%3A58\"") != -1:
-                pos = a.find( "\">" )
-                content = a[:pos] + "+720+MP4&sort_id=2" + a[pos:]
-                target.write(i[:commatwo+1] + content + '<br>')
-                rsstest = re.findall('http://[^"]*',content)
-#                if urllib.urlopen(rsstest[0]).read().find("<item>") == -1:
-#                    target.write("This rss address is wrong. Please check it manually.<br>")
-                target.write('\n')
-    #異域
-    elif i.find("team_id%3A271\"") != -1:
-        for a in rssaddress:
-            if a.find("team_id%3A271\"") != -1:
-                pos = a.find( "\">" )
-                content = a[:pos] + "+%E7%AE%80%E4%BD%93+720&sort_id=2" + a[pos:]
-                target.write(i[:commatwo+1] + content + '<br>')
-                rsstest = re.findall('http://[^"]*',content)
-#                if urllib.urlopen(rsstest[0]).read().find("<item>") == -1:
-#                    target.write("This rss address is wrong. Please check it manually.<br>")
-                target.write('\n')
-    else:
-        target2.write(i + '<br>')
-        target2.write('\n')
+items = re.findall(".{3}array\.push.*?(?=;)",pagecontent)
+blacklists = ['櫻桃小丸子','海賊王(航海王)','寶石寵物','七龍珠改魔人普烏篇',
+              '美妙旋律Pripara','火影忍者','妖怪手錶','蠟筆小新','寵物小精靈',
+              '名偵探柯南','美少女戰士Crystal','遊戲王ARC-V','偶像傳說']
+chosenteams = ['321','58','185','49','283','271']
+#ID对应'輕之國度','澄空','極影','華盟','千夏','異域'
+dict_all = {}
+dict_firstselect = {}
+dict_remain ={}
+dict_select ={}
+for i in items:
+    flag_blacklist = 1
+    for blackitem in blacklists:
+        if i.find(blackitem) != -1:
+            flag_blacklist = 0
+    if flag_blacklist == 0:
+        continue
+    weekday = conv_Weekday2Chinese(i[0:3])
+    title = re.findall("(?<=,').*?(?=',)",i)[0]
+    urls = re.findall("(?<=a href=\").*?(?=</a>)",i)
+
+    dict_item = {}
+    for url in urls:
+        team = url[url.find(">")+1:]
+        rssaddr = "http://share.dmhy.org/topics/rss/rss.xml" + url[:url.find(">")-1]
+        dict_item[team] = rssaddr
+    dict_all[weekday + ',' + title] = dict_item
+
+    flag_remain = 1
+    flag_first = 0
+    dict_item = {}
+    for chosenteam in chosenteams:
+        if i.find("team_id%3A"+chosenteam+"\"") != -1:
+            for url in urls:
+                if url.find("team_id%3A"+chosenteam+"\"") != -1:
+                    team = url[url.find(">")+1:]
+                    rssaddr = "http://share.dmhy.org/topics/rss/rss.xml" + url[:url.find(">")-1]
+                    dict_item[team] = rssaddr
+            flag_first = flag_first + 1
+            flag_remain = 0
+            if flag_first == 1:
+                dict_firstselect[weekday + ',' + title] = {team:rssaddr}
+    if flag_remain == 0:
+        dict_select[weekday + ',' + title] = dict_item
+    if flag_remain == 1:
+        dict_item = {}
+        for url in urls:
+            team = url[url.find(">")+1:]
+            rssaddr = "http://share.dmhy.org/topics/rss/rss.xml" + url[:url.find(">")-1]
+            dict_item[team] = rssaddr
+        dict_remain[weekday + ',' + title] = dict_item
+
+yaml.safe_dump({'tasks':dict_all},target_all,default_flow_style=False,allow_unicode=True)
+yaml.safe_dump({'tasks':dict_select},target_select,default_flow_style=False,allow_unicode=True)
+yaml.safe_dump({'tasks':dict_firstselect},target_firstselect,default_flow_style=False,allow_unicode=True)
+yaml.safe_dump({'tasks':dict_remain},target_remain,default_flow_style=False,allow_unicode=True)
